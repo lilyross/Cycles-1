9b23466a7c047d7ac69c5480d4e0d5ea4aebbea1
lilyross <TrasH_ThrilleR@mail.ru>
2022-09-24 09:28:11 +0300

    Домашнее задания 1 Циклы 1.1

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(" Задание 1 ");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Итерация цикла" + i);
+        }
+        System.out.println(" Задание 2 ");
+for (int i = 10; i > 0; i --) {
+    System.out.println ("Итерация цикла" + i);
+        }
+        System.out.println(" Задание 3 ");
+for (int i = 0; i < 17; i = i + 2){
+    System.out.println(" Итерация цикла " + i);
+}
+        System.out.println(" Задание 4 ");
+for (int i = 10;i >= -10;i--){
+    System.out.println(" Интерация чисел " + i);
+}
+    }
+}
